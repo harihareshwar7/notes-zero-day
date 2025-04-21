aa07906f54a1f2b90316cd2bce6dfb9bc58a1f45
Your Name <you@example.com>
2025-04-21 15:27:33 +0530

    final

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors()); // Allow all origins
 app.use(express.json());
 
 // Notes route
